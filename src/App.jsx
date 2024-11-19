bc6dd60f905340a021a6e09485d41791e6ebe287
rojhatbalta <rojhatbalta@gmail.com>
2024-11-19 16:25:23 +0300

    update homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Description from "./components/Description";
+import Options from "./components/Options";
+import Feedback from "./components/FeedBack";
+import Notification from "./components/Notification";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [feedback, setFeedback] = useState(() => {
+    const localFeedback = JSON.parse(localStorage.getItem("feedback"));
+    return localFeedback || { good: 0, neutural: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  function updateFeedback(feedbackType) {
+    setFeedback((feedback) => ({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    }));
+  }
+
+  function resetFeedback() {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  }
+
+  const totalFeedback = feedback.neutral + feedback.good + feedback.bad;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FeedBack.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+export default function Feedback({ feedback, totalFeedback }) {
+  const positiveValue = Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <div>
+      <p>Good: {feedback.good} </p>
+      <p>Neutral: {feedback.neutral} </p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback} </p>
+      <p>Positive: {positiveValue}%</p>
+    </div>
+  );
+}
+
+Feedback.propTypes = {
+  feedback: PropTypes.string.isRequired,
+  totalFeedback: PropTypes.string.isRequired,
+};
